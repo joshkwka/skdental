7bf4ca7907880ed0af177b7e60063d21d3a4887e
Josh Kwak <joshkwak01@gmail.com>
2025-01-02 00:44:27 -0800

    changing font of about section

--- skdental/components/About.tsx
+++ skdental/components/About.tsx
@@ -19,11 +19,11 @@ const About = () => {
 
       {/* Content */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-20">
-        <h1 className="text-4xl font-bold py-2 sm:text-5xl">S K Dental Ceramic Studio</h1>
-        <h2 className="text-xl font-bold italic py-1 sm:text-3xl">
+        <h1 className="text-5xl font-bold py-2">S K Dental Ceramic Studio</h1>
+        <h2 className="text-3xl font-bold italic py-1">
           Cosmetic Dental Laboratory
         </h2>
-        <p className="text-sm max-w-lg min-w-[250px] mx-auto sm:text-md">
+        <p className="text-md max-w-lg min-w-[250px] py-1 mx-auto">
           At SK Dental Ceramic Studio, we specialize in crafting high-quality cosmetic dental appliances with over 30 
           years of expertise. Our services include E-max, zirconia, porcelain, and metal crowns and bridges, implant 
           restorations, and custom night guards. Using advanced digital design, we deliver precision and excellence 
